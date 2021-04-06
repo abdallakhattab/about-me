105a3dec0a22722930ac664888e5094a7de9299f
abdallakhattab <khattababdalla@yahoo.com>
2021-04-06 17:37:01 +0300

    added some more quastions

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ let profession = prompt( ' welcome ' + username + ' my name is abdalla and i am 
 profession = profession.toLowerCase();
 alert(' you are a ' + profession + ' nice!')
 //console.log(profession)
-
+let score = 0
 let age = prompt ('how old are you?' )
 //console.log(age)
 if (age > 18 && age <28) { alert (' we are close in age i am ' + ownerage)}
@@ -16,8 +16,10 @@ let a = prompt(' doing well ??')
 a= a.toLowerCase()
 switch  (a) { 
    case 'yes' :alert ('me too am happy with my life ')
+   score++
    break;
    case 'y' : alert ('me too am happy with my life ')
+   score++
    break;
    case 'no' : alert("i am sorry")
    break;
@@ -28,8 +30,10 @@ switch  (a) {
  b=b.toLowerCase()
 switch  (b) { 
     case 'yes' :alert ('correct ')
+    score++
     break;
     case 'y' : alert ('correct ')
+    score++
     break;
     case 'no' : alert("nope")
     break;
@@ -39,8 +43,10 @@ switch  (b) {
    c=c.toLowerCase()
 switch  (c) { 
     case 'yes' :alert ('correct ')
+    score++
     break ;
     case 'y' : alert ('correct ')
+    score++
     break;
     case 'no' : alert("nope")
     break ;
@@ -51,8 +57,10 @@ let d = prompt(' do i love summer ?')
 d = d.toLowerCase()
     switch  (d) { 
         case 'yes' :alert ('correct ')
+        score++
         break;
         case 'y' : alert ('correct ')
+        score++
         break;
         case 'no' : alert("nope")
         break;
@@ -62,5 +70,31 @@ d = d.toLowerCase()
 alert( 'your name is ' + username + ' and  my name is abdalla' + " your age is  " + age + ' and i am 24' 
 + ' i am a pharmasist and you are a ' + profession 
 )
-
-
+ var num = prompt(' guess what is my favurate number from 0 to 10?');
+ 
+ for (var i= 2 ; i <5  ; i++){
+   
+if ( num === '7' ) {alert('correct great job!'); 
+score++; 
+break;}
+else if (num === '1' || num === '2' || num === '3' || num === '4' ) {var num = prompt('you are too low , try again')} 
+else if (num === '5'|| num === '6' || num === '8' || num === '9') {var num = prompt ('you are too close ! try again ')}
+else if (num === '10'){ var num = prompt('you are too high, try again')}
+}
+if (i === 5){alert(' you spent all your chances sorry the correct Answer is 7')}
+console.log(i);
+ var color = prompt('what is my favurate color? ' , 'red , black ,blue , yellow , white, orange')
+ var correctcolor = ['white' ,'blue' ,'red'];
+ console.log(correctcolor);
+ for (var q= 0 ; q <5  ; q++){
+ if (color === correctcolor[0] || color === correctcolor[1] || color === correctcolor[2]) {
+alert('correct , great job !!') ;
+score++;
+break;}
+ 
+else {color =prompt('wrong answer try again')}
+ }
+ 
+ if (q === 5){alert('you spent all your chances sorry , the correct answer was blue or red or white')}
+ 
+alert('your score is ' + score + ' outof 6')
